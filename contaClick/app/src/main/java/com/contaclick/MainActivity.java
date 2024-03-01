3917fd14e1d72e0016e6c578dd8b3565eb5ccf93
lucas guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-03-01 11:57:15 -0300

    Conta Click
    App simples de contar os clicks

---
+++ contaClick/app/src/main/java/com/contaclick/MainActivity.java
@@ -0,0 +1,31 @@
+package com.contaclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int i = 0;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.contador);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        textView.setText("0");
+    }
+
+    public void contar(View v){
+        i++;
+        TextView tv = (TextView) v;
+        tv.setText(Integer.toString(i));
+    }
+    
+}
